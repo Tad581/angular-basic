6d03ec5e18982afa0b217b89edf6ad0b56d80242
tadd582 <nguyendat58211@gmail.com>
2023-03-27 10:11:53 +0700

    chore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
+import { AddcontactComponent } from './addcontact/addcontact.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    children: [
+      { path: 'add', component: AddcontactComponent },
+      { path: 'edit/:id', component: AddcontactComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
