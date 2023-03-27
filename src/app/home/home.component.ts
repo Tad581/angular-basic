24763f81efbbc039788a566a3986781f230616ec
tadd582 <nguyendat58211@gmail.com>
2023-03-27 11:53:47 +0700

    topic: binding data & event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  headerName = 'Welcome to Angular';
 
-  ngOnInit(): void {
+  salary = 1000;
+
+  handleClick(name: string) {
+    this.headerName = name;
+    console.log('run');
   }
-
+  ngOnInit(): void {}
 }
